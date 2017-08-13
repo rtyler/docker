23c1f785b69543d36b30c45f739e2fc4e295b1c2
R Tyler Croy <tyler@monkeypox.org>
2017-08-13 08:51:02 +0800

    Forward port the changes to support reexec on FreeBSD
    Signed-off-by: R Tyler Croy <tyler@monkeypox.org>

---
+++ pkg/reexec/command_freebsd.go
@@ -0,0 +1,19 @@
+package reexec
+
+import (
+	"os/exec"
+)
+
+// Self returns the path to the current process's binary.
+
+// Uses os.Args[0].
+func Self() string {
+	return naiveSelf()
+}
+
+func Command(args ...string) *exec.Cmd {
+	return &exec.Cmd{
+		Path: Self(),
+		Args: args,
+	}
+}
