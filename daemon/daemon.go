21cb368ba637ab6e548d5ca3b068d8bfcff03861
R Tyler Croy <tyler@monkeypox.org>
2017-08-13 08:21:18 +0800

    Disable cgroup-based container security on FreeBSD until it can be made to work
    Signed-off-by: R Tyler Croy <tyler@monkeypox.org>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -714,7 +714,7 @@ func NewDaemon(config *Config, registryService *registry.Service) (daemon *Daemo
 	sysInfo := sysinfo.New(false)
 	// Check if Devices cgroup is mounted, it is hard requirement for container security,
 	// on Linux/FreeBSD.
-	if runtime.GOOS != "windows" && !sysInfo.CgroupDevicesEnabled {
+	if runtime.GOOS != "windows" && runtime.GOOS != "freebsd" && !sysInfo.CgroupDevicesEnabled {
 		return nil, fmt.Errorf("Devices cgroup isn't mounted")
 	}
 
