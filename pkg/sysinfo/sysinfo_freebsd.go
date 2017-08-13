8bdedf9ffed462a53c1e713843b4eaac76001aa9
R Tyler Croy <tyler@monkeypox.org>
2017-08-13 09:36:51 +0800

    Initialize more SysInfo structures to avoid nil de-references in the daemon
    Signed-off-by: R Tyler Croy <tyler@monkeypox.org>

--- pkg/sysinfo/sysinfo_freebsd.go
+++ pkg/sysinfo/sysinfo_freebsd.go
@@ -1,7 +1,11 @@
 package sysinfo
 
-// FREEBSD: Need to implement sysinfo checking
 func New(quiet bool) *SysInfo {
 	sysInfo := &SysInfo{}
+	/* These structs cannot be nil otherwise gnarly nil dereferences will
+	 * happen further "up" in the code (daemon.go)
+	 */
+	sysInfo.cgroupMemInfo = &cgroupMemInfo{}
+	sysInfo.cgroupCpuInfo = &cgroupCpuInfo{}
 	return sysInfo
 }
